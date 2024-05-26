67065c47522cbd5dac0aec2935b915509247a938
Seto Balian <seto.balian@gmail.com>
2024-05-26 15:25:54 +0100

    More testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,10 +14,29 @@ def test_app_for_suggestion(mocker):
     assert result.exit_code == 0
 
 
+def test_app_for_no_suggestion(mocker):
+    mocker.patch("ape.llm.call_llm", return_value="Please rephrase.")
+    result = runner.invoke(cli.app, ["what is the capital of France?"])
+    assert result.stdout == ""
+    assert result.stderr == "Please rephrase.\n"
+    assert result.exit_code == 1
+
+
 def test_app_with_empty_query():
     result = runner.invoke(cli.app, [" "])
     assert result.stdout == ""
     assert result.stderr == "Query cannot be empty.\n"
+    assert result.exit_code == 1
+
+
+def test_app_with_no_model(mocker):
+    mocker.patch(
+        "ape.llm.call_llm",
+        side_effect=llm.OpenAIAPIStatusError("model not found", 404),
+    )
+    result = runner.invoke(cli.app, ["list all the files", "--model", "abcd"])
+    assert result.stdout == ""
+    assert result.stderr == "OpenAI 404 error: model not found\n"
     assert result.exit_code == 1
 
 
@@ -26,15 +45,4 @@ def test_app_with_no_api_key(mocker):
     result = runner.invoke(cli.app, ["list all the files"])
     assert result.stdout == ""
     assert result.stderr.startswith("Generic OpenAI error:")
-    assert result.exit_code == 1
-
-
-def test_app_with_wrong_model(mocker):
-    mocker.patch(
-        "ape.llm.call_llm",
-        side_effect=llm.OpenAIAPIStatusError("model not found", 404),
-    )
-    result = runner.invoke(cli.app, ["list all the files", "--model", "abcd"])
-    assert result.stdout == ""
-    assert result.stderr == "OpenAI 404 error: model not found\n"
     assert result.exit_code == 1
